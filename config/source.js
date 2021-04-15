530eca92169142d8b02f12f1beeb69493b54de4d
Fernando Antunes <fernando.antunes1@gmail.com>
2021-04-15 16:31:40 -0300

    Criado arquivo com fonte de dados

---
+++ config/source.js
@@ -0,0 +1,124 @@
+const providers = [
+  {
+    nomeEmpresa: 'Abreutur',
+    produtoOfertado: 'Voucher de desconto',
+    categoria: 'Turismo',
+    preco: 97.00,
+    urlImagem: 'https://iguatemi.com.br/patiohigienopolis/sites/patiohigienopolis/files/logo-1360262596418.png',
+    regras: '-Utilização em 2021.\n-1 por cliente.',
+    endereco: {
+      logradouro: 'Rua Cassia Fernanda Coggo Martin',
+      numero: 12,
+      cep: '13403-677',
+      cidade: 'Piracicaba',
+      estado: 'São Paulo',
+    },
+    contato: {
+      telefone: '19-1212-2121',
+      email: 'email12@email.com',
+      whatsapp: true,
+    }
+  },
+  {
+    nomeEmpresa: 'Acqua Aroma',
+    produtoOfertado: 'Sabonetes',
+    categoria: 'Artigos do lar',
+    preco: 12.97,
+    urlImagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_rY-cdMDBnIyHkRyw-8A5nM6-2NEbs0EB_uaWaEHU1nXRq1oAMQsUs-a8mX3J32JHEq8&usqp=CAU',
+    regras: '-Válido apenas para pessoas físicas.\n-Somente enquanto durarem os estoques.',
+    endereco: {
+      logradouro: 'Rua Corina Rodrigues Sabará',
+      numero: 15,
+      cep: '13403-672',
+      cidade: 'Piracicaba',
+      estado: 'São Paulo',
+    },
+    contato: {
+      telefone: '19-1515-5151',
+      email: 'email15@email.com',
+      whatsapp: true,
+    }
+  },
+  {
+    nomeEmpresa: 'Adcos',
+    produtoOfertado: 'Shampoo',
+    categoria: 'Perfumaria e cosméticos',
+    preco: 25.98,
+    urlImagem: 'https://www.agenciadepublicidade.net.br/wp-content/uploads/2016/03/adcos.jpg',
+    regras: '-Válido apenas para pessoas físicas.\n-Somente enquanto durarem os estoques.\n-Frete grátis',
+    endereco: {
+      logradouro: 'Rua Giselda Gisele Maciel Santos',
+      numero: 18,
+      cep: '13403-676',
+      cidade: 'Piracicaba',
+      estado: 'São Paulo',
+    },
+    contato: {
+      telefone: '19-1818-8181',
+      email: 'email18@email.com',
+      whatsapp: true,
+    }
+  },
+  {
+    nomeEmpresa: 'Adidas',
+    produtoOfertado: 'Meias',
+    categoria: 'Artigos esportivos',
+    preco: 20.99,
+    urlImagem: 'https://i.pinimg.com/originals/f4/de/05/f4de052f3cc67412f510f77033d95e07.jpg',
+    regras: '-Limite de 5 unidades por CPF.\n-Somente enquanto durarem os estoques.',
+    endereco: {
+      logradouro: 'Rua José Lourenço da Silva',
+      numero: 21,
+      cep: '13403-673',
+      cidade: 'Piracicaba',
+      estado: 'São Paulo',
+    },
+    contato: {
+      telefone: '19-2121-1212',
+      email: 'email21@email.com',
+      whatsapp: false,
+    }
+  },
+  {
+    nomeEmpresa: 'Fascar',
+    produtoOfertado: 'Voucher de desconto',
+    categoria: 'Calçados masculinos',
+    preco: 59.99,
+    urlImagem: 'https://iguatemi.com.br/alphaville/sites/alphaville/files/logo-1360165533031.png',
+    regras: '-Válido apenas para Piracicaba.\n-Somente enquanto durarem os estoques.',
+    endereco: {
+      logradouro: 'Rua Getulina',
+      numero: 23,
+      cep: '13432-545',
+      cidade: 'Piracicaba',
+      estado: 'São Paulo',
+    },
+    contato: {
+      telefone: '19-2323-3232',
+      email: 'email23@email.com',
+      whatsapp: true,
+    }
+  },
+  {
+    nomeEmpresa: 'Fom',
+    produtoOfertado: 'Voucher de desconto',
+    categoria: 'Artigos do lar',
+    preco: 23.78,
+    urlImagem: 'https://baudecupons.com.br/wp-content/uploads/2019/04/cupom-fom.png',
+    regras: '-Válido apenas para peças pequenas.\n-Somente enquanto durarem os estoques.',
+    endereco: {
+      logradouro: 'Rua Macaubal',
+      numero: 25,
+      cep: '13432-593',
+      cidade: 'Piracicaba',
+      estado: 'São Paulo',
+    },
+    contato: {
+      telefone: '19-2525-5252',
+      email: 'email25@email.com',
+      whatsapp: true,
+    }
+  }
+];
+
+export default providers;
