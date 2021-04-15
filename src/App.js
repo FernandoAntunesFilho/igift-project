6065364f9877ff612af01d667ca810863a4085ba
Fernando Antunes <fernando.antunes1@gmail.com>
2021-04-15 17:19:04 -0300

    Criado componente FilterCategories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import FilterCategories from './components/filterCategories';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <h1>iGift</h1>      
+    <h1>iGift</h1>
+    <div className='filters'>
+      <FilterCategories />
+    </div>
     </div>
   );
 }
---
+++ src/components/filterCategories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class FilterCategories extends React.Component {
+  render() {
+    return(
+      <div>
+        <p>Filter</p>
+        <p>Categories</p>
+      </div>
+    )
+  }
+}
+
+export default FilterCategories;
